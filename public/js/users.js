ef31f1c9e676b239101569e9990405fd048410b7
Martín Javier <martinjavierd@gmail.com>
2023-07-28 05:50:55 +0200

    Fetch calls

--- public/js/users.js
+++ public/js/users.js
@@ -1,13 +1,13 @@
 console.log("users js");
 
-function deleteOneUser(userId) {
-  fetch(`api/users/${userId}`, { method: "DELETE" })
+async function deleteOneUser(userId) {
+  await fetch(`api/users/${userId}`, { method: "DELETE" })
     .then((response) => response.json())
     .then((data) => console.log(data));
 }
 
-function changeRole(userId) {
-  fetch(`api/users/premium/${userId}`, { method: "POST" })
+async function changeRole(userId) {
+  await fetch(`api/users/premium/${userId}`, { method: "POST" })
     .then((response) => response.json())
     .then((data) => console.log(data));
 }
